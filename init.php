43fa5cccf5f92f117fb9aaa50d54f89f532099fa
Alexey Berdnikov <aberdnikov@gmail.com>
2016-05-17 19:37:59 +0500

    Запретил складывать в билд библиотеку

--- init.php
+++ init.php
@@ -1,3 +1,7 @@
 <?php
 \Larakit\StaticFiles\Manager::package('larakit/sf-font-awesome')
-                            ->css('//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css');
+    ->css('//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css',
+        null,
+        null,
+        true
+    );
