12e53adc96f60285f93792c1bbbbcb7343a4217e
Alexey Berdnikov <aberdnikov@gmail.com>
2016-07-11 18:19:03 +0500

    fixed

--- init.php
+++ init.php
@@ -1,6 +1,7 @@
 <?php
 \Larakit\StaticFiles\Manager::package('larakit/sf-font-awesome')
-    ->css('//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css',
+    ->setSourceDir('public')
+    ->cssPackage('css/font-awesome.min.css',
         null,
         null,
         true
