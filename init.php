7916b80bc949f5e8e4bb7ec7d6f7e1e9d4ac0058
Alexey Berdnikov <aberdnikov@gmail.com>
2016-07-12 19:32:03 +0500

    fixed

--- init.php
+++ init.php
@@ -1,4 +1,4 @@
 <?php
 \Larakit\StaticFiles\Manager::package('larakit/sf-font-awesome')
     ->setSourceDir('public')
-    ->cssPackage('css/font-awesome.min.css');
+    ->cssPackage('css/font-awesome.css');
